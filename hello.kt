ba9fc21d69bebc48772c0126475fbaf388af9766
Anmol Saini <anmolsaini1234>
2026-06-30 14:18:59 +0530

    newly created kotlin file

---
+++ hello.kt
@@ -0,0 +1,21 @@
+class Car {
+    var company: String = ""
+    var model: String = ""
+    var topSpeed: Int = 0
+
+    fun addCar(company: String, model: String, topSpeed: Int) {
+        this.company = company
+        this.model = model
+        this.topSpeed = topSpeed
+    }
+}
+
+fun main() {
+    var newCar: Car = Car()
+
+    newCar.addCar("Bugatti", "V8", 244)
+
+    println(newCar.company)
+    println(newCar.model)
+    println(newCar.topSpeed)
+}
